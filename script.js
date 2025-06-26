b18ba4ac852ff8b3a3f4c6eba35c67261f421e84
mvanpractice <michaelmanggay@gmail.com>
2025-06-26 23:07:52 +0800

    Refactored getComputerPick with the help of ChatGPT

--- script.js
+++ script.js
@@ -28,15 +28,9 @@ function removeBeforeUnload() {
 */
 function getComputerPick() {
    
-   randomPick = Math.floor(Math.random() * 3);
-
-   if (randomPick === 0) {
-      return 'rock';
-   } else if (randomPick === 1) {
-      return 'paper';
-   } else if (randomPick === 2) {
-      return 'scissors';
-   }
+   const choices = ['rock', 'paper', 'scissors'];
+   const randomPick = Math.floor(Math.random() * choices.length);
+   return choices[randomPick];
 
 }
 
