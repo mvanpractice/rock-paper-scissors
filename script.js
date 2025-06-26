09bf24fc40323f57ea28c432a49849397564c030
mvanpractice <michaelmanggay@gmail.com>
2025-06-26 15:34:44 +0800

    Redesigned the UI and started adding new event I learned from chatGPT

--- script.js
+++ script.js
@@ -1,110 +1,23 @@
-
-let humanScore = 0;
-let computerScore = 0;
-
-function getHumanChoice() {
-    const input = prompt('Enter: rock, paper, scissors');
-
-    return input;
+/* 
+* param event takes an event object
+*/
+function handleBeforeUnload(event) {
+   event.preventDefault();
+   event.returnValue = '';
 }
 
+/* 
+* I put the beforeunload event listener (add and remove) in functions.
+* Because I will need to control when to trigger and remove this listener.
+*/
+function addBeforeUnload() {
 
-function getComputerChoice() {
-
-    const randomChoice = Math.floor(Math.random() * 3);
-    let result = '';
-
-    if (randomChoice === 0) {
-
-        result = 'rock';
-
-    } else if (randomChoice === 1) {
-
-        result = 'paper';
-
-    } else if (randomChoice === 2) {
-
-        result = 'scissors';
-
-    }
-
-    return result;
+   window.addEventListener('beforeunload', handleBeforeUnload);
 
 }
 
-function playRound() {
+function removeBeforeUnload() {
 
-    const humanPick = getHumanChoice();
-    const computerPick = getComputerChoice();
-
-    let winner = '';
-
-    if (humanPick === 'rock' && computerPick === 'paper') {
-
-        winner = 'computer';
-
-    } else if (humanPick === 'rock' && computerPick === 'scissors') {
-
-        winner = 'human';
-
-    } else if (humanPick === 'paper' && computerPick === 'rock') {
-
-        winner = 'human';
-
-    } else if (humanPick === 'paper' && computerPick === 'scissors') {
-
-        winner = 'computer';
-
-    } else if (humanPick === 'scissors' && computerPick === 'rock') {
-
-        winner = 'computer';
-
-    } else if (humanPick === 'scissors' && computerPick === 'paper') {
-
-        winner = 'human';
-
-    } else if (humanPick === computerPick) {
-
-        winner = 'tie';
-
-    }
-
-    return winner;
+   window.removeEventListener('beforeunload', handleBeforeUnload);
 
 }
-
-function playGame() {
-
-    for (let i = 0; i < 5; i++) {
-        
-        const roundWinner = playRound();
-
-        if (roundWinner === 'human') {
-
-            console.log('You win!');
-            humanScore++;
-
-        } else if (roundWinner === 'computer') {
-
-            console.log('You lost!');
-            computerScore++;
-
-        } else if (roundWinner === 'tie') {
-
-            console.log("It's a Tie!");
-
-        }
-        
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You WON ${humanScore}x! Congratulations! You won the entire game!`)
-    } else if (humanScore < computerScore) {
-        console.log(`You LOST! Computer won ${computerScore}x! You lost the entire game!`)
-    } else if (humanScore === computerScore) {
-        console.log('It was a TIE!');
-    }
-
-}
-
-// playGame();
